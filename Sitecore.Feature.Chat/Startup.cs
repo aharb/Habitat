7fd4786ab1c10521b8a1e5f23d5e5e6a7e0edeee
Ahmad Harb <ahmed.a.harb@gmail.com>
2016-01-30 05:07:29 +0200

    Added chat feature project

---
+++ Sitecore.Feature.Chat/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Owin;
+using Microsoft.Owin;
+
+
+[assembly: OwinStartup(typeof(Sitecore.Feature.Chat.Startup))]
+namespace Sitecore.Feature.Chat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
