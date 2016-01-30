6fa9aa6eccc41bd3e9e438cfb775eff8b4e45a42
Ahmad Harb <ahmed.a.harb@gmail.com>
2016-01-30 09:06:15 +0200

    Added SignalR start pipeline

--- src/Feature/Chat/Sitecore.Feature.Chat/Controllers/ChatController.cs
+++ src/Feature/Chat/Sitecore.Feature.Chat/Controllers/ChatController.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿
 
-namespace Sitecore.Feature.Chat.Controllers
+namespace Sitecore.Feature.Chat.Controller
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
     public class ChatController : Controller
     {
         // GET: Chat
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LiveChat()
         {
             return View();
         }
---
+++ src/Feature/Chat/Sitecore.Feature.Chat/RegisterSignalrProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Owin;
+using Microsoft.Owin;
+using Sitecore.Pipelines;
+using System.Web.Routing;
+
+[assembly: OwinStartup(typeof(Sitecore.Feature.Chat.Pipelines.RegisterSignalrProcessor))]
+namespace Sitecore.Feature.Chat.Pipelines
+{
+    public class RegisterSignalrProcessor
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+
+        public virtual void Process(PipelineArgs args)
+        {
+            
+        }
+
+    }
+}
